2a4e1c8e66ec3be06f27cc7a1f712dded85e25f5
Aryan Trivedi <aryantrivedi208@gmail.com>
2023-07-17 16:19:59 +0530

    added routing

--- src/index.js
+++ src/index.js
@@ -2,11 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import RegistrationPage from './pages/registration/RegistrationPage';
+import SelectCategory from './pages/selectCategory/SelectCategory';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/register" element=<RegistrationPage /> />
+        <Route path="/select-category" element=<SelectCategory /> />
+        <App />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
-
---
+++ src/pages/selectCategory/SelectCategory.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SelectCategory = () => {
+  return <div></div>;
+};
+
+export default SelectCategory;
