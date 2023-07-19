671dd60d6bc5f142580084c129af33626c5ceace
Aryan Trivedi <aryantrivedi208@gmail.com>
2023-07-20 00:57:02 +0530

    added browse entertainment page

--- src/pages/browsePage/BrowsePage.jsx
+++ src/pages/browsePage/BrowsePage.jsx
@@ -1,7 +1,25 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import classes from './BrowsePage.module.css';
+import Avatar from '../../assets/AvatarLogo.png';
 const BrowsePage = () => {
-  return <div></div>;
+  const [userCategories, setUserCategories] = useState(null);
+
+  useEffect(() => {
+    const storedCategories = localStorage.getItem('category');
+    if (storedCategories) {
+      setUserCategories(storedCategories);
+    }
+  });
+  return (
+    <div className={classes.BrowsePage}>
+      <div className={classes.BrowseNavigation}>
+        <h2>Superapp</h2>
+        <img src={Avatar} alt="" />
+      </div>
+      <h2>Entertainment according to your choice</h2>
+      <div></div>
+    </div>
+  );
 };
 
 export default BrowsePage;
